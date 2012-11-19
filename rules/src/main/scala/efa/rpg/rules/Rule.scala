bc6594b4c6bf17288a9dc6f64e845f8d0278e922
Stefan Hoeck <hock@zhaw.ch>
2012-11-19 15:28:16 +0100

    Rule from State

--- rules/src/main/scala/efa/rpg/rules/Rule.scala
+++ rules/src/main/scala/efa/rpg/rules/Rule.scala
@@ -1,10 +1,15 @@
 package efa.rpg.rules
 
-import scalaz.Endo
+import scalaz.{Endo, State}
 
 case class Rule[A](id: String, f: A ⇒ A) {
   def endo (ids: Set[String]): Endo[A] =
     if (ids(id)) Endo endo f else Endo.idEndo
+}
+
+object Rule {
+  def state[A] (id: String, s: State[A,Unit]): Rule[A] =
+    Rule[A] (id, s exec _)
 }
 
 // vim: set ts=2 sw=2 et:
