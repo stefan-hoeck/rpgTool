49b716075db72cede4c27822c6c24eb998c3bdb7
efasckenoth <efasckenoth@gmail.com>
2012-11-19 07:19:31 +0100

    MVPanel extends PersistentComponent and Lookup.Provider

--- being/src/main/scala/efa/rpg/being/MVPanel.scala
+++ being/src/main/scala/efa/rpg/being/MVPanel.scala
@@ -1,8 +1,12 @@
 package efa.rpg.being
 
-trait MVPanel[A,B] extends BeingPanel[A,B] {
-  def prefId: String
-  def locName: String
-}
+import efa.nb.tc.PersistentComponent
+import org.openide.util.Lookup
+
+abstract class MVPanel[A,B] (
+  val prefId: String,
+  val locName: String,
+  val version: String
+) extends BeingPanel[A,B] with Lookup.Provider with PersistentComponent
 
 // vim: set ts=2 sw=2 et:
