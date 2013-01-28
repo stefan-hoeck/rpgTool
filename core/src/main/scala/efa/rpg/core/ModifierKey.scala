20422ed5ecc07dc326128705eacc29de86cf3bbd
efasckenoth <efasckenoth@gmail.com>
2013-01-28 05:45:06 +0100

    adjusted to shapeless lenses

--- core/src/main/scala/efa/rpg/core/ModifierKey.scala
+++ core/src/main/scala/efa/rpg/core/ModifierKey.scala
@@ -1,12 +1,14 @@
 package efa.rpg.core
 
-import efa.core.Localization
-import scalaz.{Equal,Scalaz}
+import efa.core.{Localization, Efa}, Efa._
+import scalaz._, Scalaz._
+import shapeless.Iso
 
 case class ModifierKey(loc: Localization, min: Long, max: Long) {
   override def toString = loc.locName
 }
 
 object ModifierKey {
-  implicit val ModifierKeyEqual: Equal[ModifierKey] = Equal.equalA
+  implicit val MIso = Iso.hlist(ModifierKey.apply _, ModifierKey.unapply _)
+  implicit val ModifierKeyEqual: Equal[ModifierKey] = ccEqual
 }
