29b994928f265a94a1d6c9d7db9909786cba79a6
efasckenoth <efasckenoth@gmail.com>
2013-02-04 18:14:21 +0100

    fixed tests for DieRoller

--- core/src/test/scala/efa/rpg/core/DieRollerTest.scala
+++ core/src/test/scala/efa/rpg/core/DieRollerTest.scala
@@ -4,14 +4,15 @@ import efa.rpg.core.specs.ReadProps
 import org.scalacheck.Prop
 
 object DieRollerTest extends ReadProps[DieRoller]("DieRoller") {
-  
   import DieRoller._
+  
+  val L = scalaz.Lens.self[DieRoller]
 
-  property("valdateCount") = Prop forAll validated(count.set)(countVal)
+  property("valdateCount") = Prop forAll validated(L.count.set)(countVal)
 
-  property("valdateDie") = Prop forAll validated(die.set)(dieVal)
+  property("valdateDie") = Prop forAll validated(L.die.set)(dieVal)
 
-  property("valdatePlus") = Prop forAll validated(plus.set)(plusVal)
+  property("valdatePlus") = Prop forAll validated(L.plus.set)(plusVal)
 }
 
 // vim: set ts=2 sw=2 et:
