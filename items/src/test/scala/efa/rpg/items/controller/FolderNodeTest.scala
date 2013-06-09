79211e119d91466950fbd26d93ca6fb7b5503f86
Stefan Hoeck <hock@zhaw.ch>
2013-06-09 13:58:01 +0200

    Reimplemented FolderNode tests

--- items/src/test/scala/efa/rpg/items/controller/FolderNodeTest.scala
+++ items/src/test/scala/efa/rpg/items/controller/FolderNodeTest.scala
@@ -1,91 +1,87 @@
 package efa.rpg.items.controller
 
-//import efa.core.{Folder, Default}
-//import efa.nb.node.NbNode
-//import efa.rpg.items._, ItemsStateTest.IsArbitrary
-//import efa.react.{SIn}
-//import org.openide.nodes.Node
-//import org.scalacheck._, Prop._
-//import scalaz._, Scalaz._, effect.IO
-//
-//object FolderNodeTest
-//   extends Properties("FolderNode")
-//   with FolderFunctions 
-//   with TreeFunctions {
-//
-//  lazy val advOut: ItemNodes.FullOut[Advantage] =
-//    ItemNodes.name[Advantage] ∙ (_._1)
-//
-//  property("name") = forAll { s: IState[Advantage] ⇒ 
-//    def res = for {
-//      n  ← NbNode.apply
-//      no = FolderNode.name[Advantage] set n
-//      _  ← sin(s) andThen no go
-//    } yield n.getDisplayName ≟ IState.root.name.get(s)
-//
-//    evalIO (res)
-//  }
-//
-//  property("children") = forAll { s: IState[Advantage] ⇒ 
-//    def res = for {
-//      n  ← NbNode.apply
-//      no = try{FolderNode.defaultOut[Advantage] (advOut, Nil)} catch {
-//             case e: StackOverflowError ⇒ e.printStackTrace; throw e
-//           }
-//      _  ← sin(s) andThen no.set(n) runIO ()
-//      fNames = folderToNames (s.root)
-//      nNames = nodeToNames (n)
-//    } yield fNames ≟ nNames
-//
-//    evalIO (res)
-//  }
-//
-//  val emptyFA: NameFolder[Advantage] =
-//    Folder(Stream.empty, Stream.empty, loc.folder)
-//
-//  val as = Stream.fill(2000)(Default.!!![Advantage]).zipWithIndex map {
-//    case (a,i) ⇒ Advantage.data.id.set(a, i)}
-//
-//  val itemsWide: NameFolder[Advantage] =
-//    Folder(as, Stream.empty, loc.folder)
-//
-//  property("itemsWide") = {
-//    val s = IState fromFolder itemsWide
-//    def res = for {
-//      n  ← NbNode.apply
-//      no = try{FolderNode.defaultOut[Advantage] (advOut, Nil)} catch {
-//             case e: StackOverflowError ⇒ e.printStackTrace; throw e
-//           }
-//      _  ← sin(s) andThen no.set(n) runIO ()
-//      cCount = n.getChildren.getNodes(true).size
-//      fNames = folderToNames (s.root)
-//      nNames = nodeToNames (n)
-//    } yield (fNames ≟ nNames) && (cCount ≟ as.size)
-//
-//    evalIO (res)
-//  }
-//
-//  def sin (s: IState[Advantage]): SIn[FolderPair[Advantage]] =
-//    (s.root, s).η[SIn]
-//
-//  private def evalIO (io: IO[Boolean]) = io.unsafePerformIO
-//
-//  def folderToNames (f: IFolder[Advantage]): Tree[String] = {
-//    lazy val itemNames =
-//      f.data.sortWith(_.name < _.name) ∘ (a ⇒ leaf (a.name))
-//    lazy val folderNames =
-//      f.folders.sortBy(_.label._1) ∘ (folderToNames)
-//
-//    node (f.label._1, folderNames #::: itemNames)
-//  }
-//
-//  def nodeToNames (n: Node): Tree[String] = {
-//    def children = n.getChildren getNodes true toStream
-//    def childNames = children ∘ (nodeToNames)
-//
-//    node (n.getDisplayName, childNames)
-//  }
-//
-//}
+import dire.{SF, SIn}
+import efa.core.{Folder, Default}
+import efa.nb.node.NbNode
+import efa.rpg.items._, ItemsStateTest.IsArbitrary
+import org.openide.nodes.Node
+import org.scalacheck._, Prop._
+import scalaz._, Scalaz._, effect.IO
+
+object FolderNodeTest
+   extends Properties("FolderNode")
+   with FolderFunctions 
+   with TreeFunctions 
+   with dire.util.TestFunctions {
+
+  lazy val advOut: ItemNodes.FullOut[Advantage] =
+    ItemNodes.name[Advantage] ∙ (_._1)
+
+  property("name") = forAll { s: IState[Advantage] ⇒ 
+    def res = for {
+      n  ← NbNode.apply
+      sf = sin(s) andThen FolderNode.name[Advantage].sf(n)
+      _  = runN(sf, 0)
+    } yield n.getDisplayName ≟ IState.root.name.get(s)
+
+    evalIO (res)
+  }
+
+  property("children") = forAll { s: IState[Advantage] ⇒ 
+    def res = for {
+      n  ← NbNode.apply
+      sf = sin(s) andThen FolderNode.defaultOut[Advantage](advOut, Nil).sf(n)
+      _  = runN(sf, 0)
+      fNames = folderToNames(s.root)
+      nNames = nodeToNames(n)
+    } yield fNames ≟ nNames
+
+    evalIO (res)
+  }
+
+  val emptyFA: NameFolder[Advantage] =
+    Folder(Stream.empty, Stream.empty, loc.folder)
+
+  val as = Stream.fill(2000)(Default.!!![Advantage]).zipWithIndex map {
+    case (a,i) ⇒ Advantage.data.id.set(a, i)}
+
+  val itemsWide: NameFolder[Advantage] =
+    Folder(as, Stream.empty, loc.folder)
+
+  property("itemsWide") = {
+    val s = IState fromFolder itemsWide
+    def res = for {
+      n  ← NbNode.apply
+      sf = sin(s) andThen FolderNode.defaultOut[Advantage](advOut, Nil).sf(n)
+      _  = runN(sf, 0)
+      cCount = n.getChildren.getNodes(true).size
+      fNames = folderToNames(s.root)
+      nNames = nodeToNames(n)
+    } yield (fNames ≟ nNames) && (cCount ≟ as.size)
+
+    evalIO (res)
+  }
+
+  def sin(s: IState[Advantage]): SIn[FolderPair[Advantage]] =
+    SF const (s.root, s)
+
+  private def evalIO (io: IO[Boolean]) = io.unsafePerformIO
+
+  def folderToNames (f: IFolder[Advantage]): Tree[String] = {
+    lazy val itemNames =
+      f.data.sortWith(_.name < _.name) ∘ (a ⇒ leaf (a.name))
+    lazy val folderNames =
+      f.folders.sortBy(_.label._1) ∘ (folderToNames)
+
+    node (f.label._1, folderNames #::: itemNames)
+  }
+
+  def nodeToNames (n: Node): Tree[String] = {
+    def children = n.getChildren getNodes true toStream
+    def childNames = children ∘ (nodeToNames)
+
+    node (n.getDisplayName, childNames)
+  }
+}
 
 // vim: set ts=2 sw=2 et:
