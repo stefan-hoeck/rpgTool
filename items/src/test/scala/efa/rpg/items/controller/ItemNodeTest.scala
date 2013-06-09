47edeffa2b9998f84f25f3b73ec4967a14117ab2
Stefan Hoeck <hock@zhaw.ch>
2013-06-09 14:51:57 +0200

    Reimplemented ItemNode tests

--- items/src/test/scala/efa/rpg/items/controller/ItemNodeTest.scala
+++ items/src/test/scala/efa/rpg/items/controller/ItemNodeTest.scala
@@ -1,61 +1,68 @@
 package efa.rpg.items.controller
 
-//import efa.core._, Efa._
-//import efa.nb.node.NbNode
-//import efa.react._
-//import efa.rpg.items._
-//import efa.rpg.core.{HtmlDesc}
-//import org.openide.cookies.EditCookie
-//import org.scalacheck._, Prop._
-//import scalaz._, Scalaz._, effect.IO
-//
-//object ItemNodeTest extends Properties ("ItemNode") with FolderFunctions {
-//
-//  type AState = ValRes[State[IState[Advantage], Unit]]
-//
-//  lazy val itemOut = ItemNodes.defaultOut[Advantage]
-//
-//  property ("defaultOut") = forAll { a: Advantage ⇒ 
-//    val pair = itemToPair(a)
-//    val hd = HtmlDesc(a.name, a.desc)
-//
-//    val res = for {
-//      n   ← NbNode.apply
-//      _   ← pair.η[SIn] andThen itemOut.set(n) runIO ()
-//      ec  ← n.getLookup.head[EditCookie]
-//      hdo ← n.getLookup.head[HtmlDesc]
-//    } yield (n.getDisplayName ≟ a.name) :| "display name" &&
-//            (n.getShortDescription ≟ a.desc) :| "shortDesc" &&
-//            (hdo ≟ hd.some) :| "htmlDesc" &&
-//            ec.nonEmpty :| "edit cookie" &&
-//            n.canDestroy :| "delete" &&
-//            n.canRename :| "rename"
-//
-//    evalPropIO (res)
-//  }
-//
-//  val nesVal = Validators.notEmptyString
-//
-//  lazy val renameOut = ItemNodes.rename[Advantage](_ ⇒ nesVal)
-//
-//  property ("rename") = forAll { p: (Advantage,String) ⇒ 
-//    val (a, s) = p
-//    val pair = itemToPair(a)
-//
-//    val res = for {
-//      stRef ← IO newIORef "".failureNel[St[Advantage]]
-//      n     ← NbNode.apply // new node
-//      _     ← pair.η[SIn] andThen renameOut.set(n) to (stRef write _) runIO ()
-//      _     = n.setName (s) // change node's name. This should set stRef
-//      v     ← stRef.read
-//    } yield (nesVal(s).isRight ≟ v.isSuccess)
-//
-//    evalIO (res)
-//  }
-//
-//  private def evalIO (io: IO[Boolean]) = io.unsafePerformIO
-//
-//  private def evalPropIO (io: IO[Prop]) = io.unsafePerformIO
-//}
+import dire.{SIn, SF, Out}
+import efa.core._, Efa._
+import efa.nb.node.{NbNode, NodeOut}
+import efa.rpg.items._
+import efa.rpg.core.{HtmlDesc}
+import org.openide.cookies.EditCookie
+import org.scalacheck._, Prop._
+import scalaz._, Scalaz._, effect.IO
+
+object ItemNodeTest extends Properties ("ItemNode")
+  with FolderFunctions 
+  with dire.util.TestFunctions {
+
+  type AState = ValRes[State[IState[Advantage], Unit]]
+
+  lazy val itemOut = ItemNodes.defaultOut[Advantage]
+
+  property ("defaultOut") = forAll { a: Advantage ⇒ 
+    val pair = itemToPair(a)
+    val hd = HtmlDesc(a.name, a.desc)
+
+    val res = for {
+      n   ← NbNode.apply
+      _   = runN(pair.η[SIn] andThen itemOut.sf(n), 0)
+      ec  ← n.getLookup.head[EditCookie]
+      hdo ← n.getLookup.head[HtmlDesc]
+    } yield (n.getDisplayName ≟ a.name) :| "display name" &&
+            (n.getShortDescription ≟ a.desc) :| "shortDesc" &&
+            (hdo ≟ hd.some) :| "htmlDesc" &&
+            ec.nonEmpty :| "edit cookie" &&
+            n.canDestroy :| "delete" &&
+            n.canRename :| "rename"
+
+    evalPropIO (res)
+  }
+
+  val nesVal = Validators.notEmptyString
+
+  lazy val renameOut = ItemNodes.rename[Advantage](_ ⇒ nesVal)
+
+  property ("rename") = forAll { p: (Advantage,String) ⇒ 
+    val (a, s) = p
+
+    simulate(List(s), true)(eventSf(a)) ∀ { _.isSuccess ≟ nesVal(s).isRight }
+  }
+
+  private def evalIO (io: IO[Boolean]) = io.unsafePerformIO
+
+  private def evalPropIO (io: IO[Prop]) = io.unsafePerformIO
+
+  private def eventSf(a: Advantage)(o: Out[Unit])
+    : IO[SF[String,VSt[Advantage]]] = for {
+      n  ← NbNode()
+      sf ← IO {
+             def onE(s: String): IO[Unit] = IO(n.setName(s))
+
+             val nodeOut: NodeOut[Unit,VSt[Advantage]] = 
+               renameOut.contramap{ _: Unit ⇒ itemToPair(a) } ⊹
+               NodeOut[Unit,VSt[Advantage]]((_,_) ⇒ o)
+             
+             (SF.id[String] syncTo onE) >> (SF.once(()) >=> nodeOut.sf(n))
+           }
+    } yield sf
+}
 
 // vim: set ts=2 sw=2 et:
