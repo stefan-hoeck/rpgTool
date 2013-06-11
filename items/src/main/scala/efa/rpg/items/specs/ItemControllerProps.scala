1701023f7c4359801ef127c9b80baee19291879c
Stefan Hoeck <hock@zhaw.ch>
2013-06-11 15:51:03 +0200

    Added trait for testing ItemControllers

---
+++ items/src/main/scala/efa/rpg/items/specs/ItemControllerProps.scala
@@ -0,0 +1,17 @@
+package efa.rpg.items.specs
+
+import efa.rpg.items.controller.ItemController
+import org.scalacheck._, Prop._
+import scalaz._, Scalaz._, effect.IO
+
+trait ItemControllerProps extends dire.util.TestFunctions {
+
+  def testLoading[A](ic: ItemController[A]): Prop =
+    runN(ic.dbIn, 1) match {
+      case Nil ⇒ false :| "Returned empty list"
+      case m :: Nil ⇒ m.nonEmpty :| "Returned empty map"
+      case x ⇒ false :| "Returned more than one map"
+    }
+}
+
+// vim: set ts=2 sw=2 et:
