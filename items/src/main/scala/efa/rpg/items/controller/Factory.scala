82af7be4ff271f657630ebcbf19c059ae1de2446
Stefan Hoeck <hock@zhaw.ch>
2013-06-11 15:31:06 +0200

    Added factory trait for ItemControllers
    This is useful for creating singleton ItemControllers
    (most if not all such controllers will be singletons)

---
+++ items/src/main/scala/efa/rpg/items/controller/Factory.scala
@@ -0,0 +1,21 @@
+package efa.rpg.items.controller
+
+import efa.core.ToXml
+import efa.rpg.core.RpgItem
+import scala.reflect.runtime.universe.TypeTag
+import scalaz.Equal
+
+/** The factory methods provided in this trait are not referentially
+  * transparent. This trait should therefore only be used to extend
+  * singleton objects that define lazy vals using the functions provided
+  * in this trait.
+  */
+trait Factory {
+  private lazy val clazz = getClass
+
+  def singleton[A:RpgItem:Equal:ToXml:Manifest:IEditable:TypeTag](
+    p: (String, String)): ItemController[A] =
+    ItemController.default[A](p._1, p._2, clazz).unsafePerformIO()
+}
+
+// vim: set ts=2 sw=2 et:
